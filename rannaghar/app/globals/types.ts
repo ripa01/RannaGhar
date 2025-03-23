b3f6e0204190ab3fc5d31687cb9b9cedfa37dc24
ripa01 <ripabiswas2739@gmail.com>
2025-03-24 03:26:05 +0600

    Add TypeScript interfaces for Category and Meal

---
+++ rannaghar/app/globals/types.ts
@@ -0,0 +1,12 @@
+export interface Category {
+    idCategory: string; 
+    strCategory: string; 
+    strCategoryThumb: string; 
+    strCategoryDescription: string; 
+  }
+  
+export interface Meal {
+    strMeal: string; 
+    strMealThumb: string; 
+    idMeal: string; 
+  }
